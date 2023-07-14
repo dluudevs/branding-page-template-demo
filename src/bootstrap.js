e9a493bfd9055e855e3cd9be06148c6b5ae15464
Derek Luu <dluu@indocresearch.org>
2023-07-14 13:39:37 -0400

    add missing style sheets

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Auth from "./Views/Login/Auth";
 
+import "./Antd.less";
+import "./bootstrap.scss";
+
 ReactDOM.render(<Auth />, document.getElementById("root"));
