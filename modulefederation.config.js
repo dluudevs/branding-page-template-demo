67b2368cad155693e97100857c6da9efd362a8f3
Derek Luu <dluu@indocresearch.org>
2023-07-17 16:48:40 -0400

    remove shared deps, add portal job status as remote

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -1,34 +1,12 @@
-const deps = require("./package.json").dependencies;
+const remotePortal = process.env.REACT_APP_REMOTE_MODULE_PILOT;
 
 module.exports = {
   name: "branding",
   filename: "remoteEntry.js",
   remotes: {
-    pilot: "pilot@http://localhost:3000/remoteEntry.js",
+    portal: `portal@${remotePortal}/remoteEntry.js`,
   },
   exposes: {
-    "./Auth": "./src/Views/Login/Auth.jsx",
-  },
-  shared: {
-    ...deps,
-    react: {
-      singleton: true,
-      requiredVersion: deps["react"],
-    },
-    "react-dom": {
-      singleton: true,
-      requiredVersion: deps["react-dom"],
-    },
-    "react-router-dom": {
-      singleton: true,
-      requiredVersion: deps["react-router-dom"],
-      shareScope: "react-router-dep",
-      shareKey: "react-router-dep-react-router-dom",
-    },
-    "path-to-regexp": {
-      requiredVersion: "1.8.0",
-      shareScope: "react-router-dep",
-      shareKey: "react-router-dep-path-to-regexp",
-    },
+    "./Auth": "./src/exposeComponent.js",
   },
 };
